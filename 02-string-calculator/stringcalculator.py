924716277a6750ab53b6f16e6f71e0bd0900ba18
Samuele Mattiuzzo <samumatt@gmail.com>
2022-04-16 14:23:07 +0100

    Stringcalculator update: custom delimiters

--- 02-string-calculator/stringcalculator.py
+++ 02-string-calculator/stringcalculator.py
@@ -1,16 +1,19 @@
 import re
 
+ERROR_MESSAGE = 'ERROR: The input cannot end with a delimiter'
+
 
 def stringcalculator(numbers):
     if not len(numbers):
         # empty string
         return 0
 
-    split_input = re.split(',|\n', numbers)
-    if split_input[-1] == '':
+    separator, numbers = re.split('//|\n', numbers)[1:]
+
+    if numbers.endswith(separator):
         # the input ended with a delimiter
-        raise Exception('The input cannot end with a delimiter')
+        raise Exception(ERROR_MESSAGE)
 
+    split_numbers = numbers.split(separator)
     # return the sum
-    return sum(map(int, split_input))
-
+    return sum(map(int, split_numbers))
--- 02-string-calculator/test.py
+++ 02-string-calculator/test.py
@@ -1,28 +1,29 @@
 import unittest
 
-from stringcalculator import stringcalculator
+from stringcalculator import ERROR_MESSAGE, stringcalculator
 
 
 class TestStringcalculator(unittest.TestCase):
-    
+
     def test_should_return_zero_if_empty_string(self):
         self.assertEqual(stringcalculator(''), 0)
 
     def test_should_return_number_if_only_one_present(self):
-        self.assertEqual(stringcalculator('1'), 1)
+        self.assertEqual(stringcalculator('//,\n1'), 1)
 
     def test_should_return_sum_of_two_numbers(self):
-        self.assertEqual(stringcalculator('1,2'), 3)
+        self.assertEqual(stringcalculator('//,\n1,2'), 3)
 
     def test_should_return_sum_of_all_numbers(self):
-        self.assertEqual(stringcalculator('1,2,3'), 6)
-
-    def test_should_return_sum_of_all_numbers_with_newline_delimiter(self):
-        self.assertEqual(stringcalculator('1,2\n3'), 6)
+        self.assertEqual(stringcalculator('//,\n1,2,3'), 6)
 
     def test_should_raise_if_input_ends_with_delimiter(self):
-        self.assertRaises(Exception, stringcalculator, '1,2,3,' )
+        with self.assertRaises(Exception):
+            stringcalculator('//,\n1,2,3,')
+
+    def test_should_allow_custom_separator(self):
+        self.assertEqual(stringcalculator('//,\n1,2,3'), 6)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
