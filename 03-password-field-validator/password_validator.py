4ac899db614c5758cd3d6fc87c89dba9c007345f
Samuele Mattiuzzo <samumatt@gmail.com>
2022-04-17 01:15:36 +0100

    03: Password field validator

--- 03-password-field-validator/password_validator.py
+++ 03-password-field-validator/password_validator.py
@@ -1,10 +1,32 @@
+import re
+from curses import ERR
+
 ERROR_MESSAGES = {
-    'LENGTH': 'Password must be at least 8 characters'
+    'LENGTH': 'Password must be at least 8 characters',
+    'NUMBERS': 'Password must contain at least 2 numbers',
+    'CAPITAL': 'Password must contain at least one capital letter',
+    'SPECIAL': 'Password must contain at least one special character'
 }
+
+VALID_PASSWORD_LENGTH = 8
+TWO_NUMBERS_REGEX = r'\d.*\d'
+CAPITAL_LETTER_REGEX = r'[A-Z]'
+SPECIAL_CHAR_REGEX = r'[^a-zA-Z\d]'
 
 
 def password_validator(password):
-    if len(password) < 8:
-        return (False, ERROR_MESSAGES['LENGTH'])
+    errors = []
 
-    return (True, None)
+    if len(password) < VALID_PASSWORD_LENGTH:
+        errors.append(ERROR_MESSAGES['LENGTH'])
+
+    if not bool(re.search(TWO_NUMBERS_REGEX, password)):
+        errors.append(ERROR_MESSAGES['NUMBERS'])
+
+    if not bool(re.search(CAPITAL_LETTER_REGEX, password)):
+        errors.append(ERROR_MESSAGES['CAPITAL'])
+
+    if not bool(re.search(SPECIAL_CHAR_REGEX, password)):
+        errors.append(ERROR_MESSAGES['SPECIAL'])
+
+    return (len(errors) == 0, errors)
--- 03-password-field-validator/test.py
+++ 03-password-field-validator/test.py
@@ -6,16 +6,26 @@ from password_validator import ERROR_MESSAGES, password_validator
 class TestPasswordFieldValidator(unittest.TestCase):
 
     def setUp(self):
-        self.valid_password = 'iamvalid'
-        self.invalid_password = 'not'
+        self.valid_password = '1aMval1d_'
+        self.invalid_password = 'not1'
 
-    def test_should_return_true_if_password_is_eight_chars_long(self):
+    def test_should_return_true_if_password_matches_conditions(self):
+        valid, errors = password_validator(self.valid_password)
 
-        self.assertTrue(password_validator(self.valid_password)[0])
+        self.assertTrue(valid)
+        self.assertFalse(len(errors))
 
-        self.assertFalse(password_validator(self.invalid_password)[0])
-        self.assertEqual(password_validator(self.invalid_password)[
-            1], ERROR_MESSAGES['LENGTH'])
+    def test_should_contain_all_error_messages_if_multiple(self):
+        valid, errors = password_validator(self.invalid_password)
+
+        self.assertFalse(valid)
+        self.assertEqual(len(errors), len(ERROR_MESSAGES))
+
+        joined_errors = '|'.join([err for err in errors])
+        self.assertIn(ERROR_MESSAGES['LENGTH'], joined_errors)
+        self.assertIn(ERROR_MESSAGES['NUMBERS'], joined_errors)
+        self.assertIn(ERROR_MESSAGES['CAPITAL'], joined_errors)
+        self.assertIn(ERROR_MESSAGES['SPECIAL'], joined_errors)
 
 
 if __name__ == '__main__':
