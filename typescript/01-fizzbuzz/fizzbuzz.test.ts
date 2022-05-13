80fffab84818c6ba34d7e764d74f30da308a6f90
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-13 10:40:15 +0100

    TS Fizzbuzz: Fizz test case

--- typescript/01-fizzbuzz/fizzbuzz.test.ts
+++ typescript/01-fizzbuzz/fizzbuzz.test.ts
@@ -4,4 +4,8 @@ describe('test FizzBuzz implementation', () => {
   it('should return a valid number', () => {
     expect(FizzBuzz(1)).toEqual("1");
   })
+
+  it('should return Fizz for multiples of 3', () => {
+    expect(FizzBuzz(3)).toEqual("Fizz");
+  })
 })
--- typescript/01-fizzbuzz/fizzbuzz.ts
+++ typescript/01-fizzbuzz/fizzbuzz.ts
@@ -1,4 +1,5 @@
 
 export function FizzBuzz(num: number): string {
+  if (num % 3 === 0) return "Fizz";
   return num.toString();
 }
