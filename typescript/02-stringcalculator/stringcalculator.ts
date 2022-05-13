2cad31fea584f7d80d420d8efcf62920ea532977
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-13 12:04:29 +0100

    TS Stringcalculator: arbitrary delimiter case -> refactor

--- typescript/02-stringcalculator/stringcalculator.ts
+++ typescript/02-stringcalculator/stringcalculator.ts
@@ -1,19 +1,17 @@
 
 export function StringCalculator(input: string): number {
-  if (input.endsWith(',') || input.endsWith('\n')) {
-    throw new Error("Error: the input can't end with a delimiter");
-  }
-
   if (input === "") return 0;
 
   // we need filter to remove empty values
   var splitted = input.split(/\n/),
-      sum = 0,
-      delimiter = '',
-      values = [];
+      sum = 0;
 
-  delimiter = splitted[0].replace('//', '');
-  values = splitted[1].split(delimiter).filter(c => c);
+  var delimiter = splitted[0].replace('//', ''),
+      values = splitted[1].split(delimiter).filter(c => c);
+
+    if (input.endsWith(delimiter)) {
+      throw new Error("Error: the input can't end with a delimiter");
+    }
 
   for (var i = 0; i < values.length; i++) {
     sum += +values[i];
