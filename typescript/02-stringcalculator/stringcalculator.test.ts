e470b57b8e3c23608bf828e4eabf486aac1b2205
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-13 10:54:58 +0100

    TS Stringcalculator: 2-length case

--- typescript/02-stringcalculator/stringcalculator.test.ts
+++ typescript/02-stringcalculator/stringcalculator.test.ts
@@ -8,4 +8,8 @@ describe('test StringCalculator implementation', () => {
   it('should return the number if the input contains 1 number', () => {
     expect(StringCalculator("1")).toEqual(1);
   })
+
+  it('should return the sum of number in comma-separated input', () => {
+    expect(StringCalculator("1,2")).toEqual(3);
+  })
 })
--- typescript/02-stringcalculator/stringcalculator.ts
+++ typescript/02-stringcalculator/stringcalculator.ts
@@ -3,4 +3,7 @@ export function StringCalculator(input: string): number {
   if (input === "") return 0;
 
   if (input.length === 1) return +input;
+
+  var values = input.split(',');
+  return +values[0] + +values[1];
 }
