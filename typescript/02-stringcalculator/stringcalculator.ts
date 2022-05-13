734e28a54bfb5766d9323dae2eafa5dd99e91aaf
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-13 10:58:43 +0100

    TS Stringcalculator: arbitrary length case -> refactor

--- typescript/02-stringcalculator/stringcalculator.ts
+++ typescript/02-stringcalculator/stringcalculator.ts
@@ -2,12 +2,12 @@
 export function StringCalculator(input: string): number {
   if (input === "") return 0;
 
-  if (input.length === 1) return +input;
+  var values = input.split(','),
+      sum = 0;
 
-  var values = input.split(',');
-  var sum = 0;
   for (var i = 0; i < values.length; i++) {
     sum += +values[i];
   }
+  
   return sum;
 }
