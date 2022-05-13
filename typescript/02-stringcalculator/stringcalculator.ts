d93c45c5d27550ae8d6d5bdf60d66c1dcb8d5e71
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-13 12:07:50 +0100

    TS Stringcalculator: indentation

--- typescript/02-stringcalculator/stringcalculator.ts
+++ typescript/02-stringcalculator/stringcalculator.ts
@@ -9,9 +9,9 @@ export function StringCalculator(input: string): number {
   var delimiter = splitted[0].replace('//', ''),
       values = splitted[1].split(delimiter).filter(c => c);
 
-    if (input.endsWith(delimiter)) {
-      throw new Error("Error: the input can't end with a delimiter");
-    }
+  if (input.endsWith(delimiter)) {
+    throw new Error("Error: the input can't end with a delimiter");
+  }
 
   values.forEach(x => { sum += +x; });
 
