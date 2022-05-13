f9bd8632dc330295fc02b5d0a8cb7caac180032c
Samuele Mattiuzzo <samumatt@gmail.com>
2022-05-13 11:15:40 +0100

    TS Stringcalculator: mixed delimiters case

--- typescript/02-stringcalculator/stringcalculator.test.ts
+++ typescript/02-stringcalculator/stringcalculator.test.ts
@@ -16,4 +16,8 @@ describe('test StringCalculator implementation', () => {
   it('should return the sum of all numbers in comma-separated input', () => {
     expect(StringCalculator("1,2,3")).toEqual(6);
   })
+
+  it('should return the sum of all numbers with mixed delimiters in input', () => {
+    expect(StringCalculator("1,2\n3")).toEqual(6);
+  })
 })
--- typescript/02-stringcalculator/stringcalculator.ts
+++ typescript/02-stringcalculator/stringcalculator.ts
@@ -2,12 +2,13 @@
 export function StringCalculator(input: string): number {
   if (input === "") return 0;
 
-  var values = input.split(','),
+  // we need filter to remove empty values
+  var values = input.split(/,|\n/).filter(c => c),
       sum = 0;
 
   for (var i = 0; i < values.length; i++) {
     sum += +values[i];
   }
-  
+
   return sum;
 }
